2fcb8afe4364662d2787e4ff5e98b030ec5fde38
imsyf <alghozalyi@gmail.com>
2022-07-08 17:20:53 +0700

    Add logic and interactivity to the layout

--- DiceRoller/src/main/java/im/syf/diceroller/MainActivity.kt
+++ DiceRoller/src/main/java/im/syf/diceroller/MainActivity.kt
@@ -1,11 +1,21 @@
 package im.syf.diceroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text: TextView = findViewById(R.id.text)
+        val rollButton: Button = findViewById(R.id.button)
+        val dice = Dice(6)
+
+        rollButton.setOnClickListener {
+            text.text = "${dice.roll()}"
+        }
     }
 }
