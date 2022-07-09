06b192adba3dbd2cfb23bd801001c3492f3501e1
imsyf <alghozalyi@gmail.com>
2022-07-09 11:06:16 +0700

    Display roll result as image instead of text

--- DiceRoller/src/main/java/im/syf/diceroller/MainActivity.kt
+++ DiceRoller/src/main/java/im/syf/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package im.syf.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +10,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text: TextView = findViewById(R.id.text)
+        val image: ImageView = findViewById(R.id.image)
         val rollButton: Button = findViewById(R.id.button)
         val dice = Dice(6)
 
         rollButton.setOnClickListener {
-            text.text = "${dice.roll()}"
+            val side = dice.roll()
+            val id = when (side) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+
+            image.setImageResource(id)
+            image.contentDescription = getString(R.string.dice_side, side)
         }
     }
 }
