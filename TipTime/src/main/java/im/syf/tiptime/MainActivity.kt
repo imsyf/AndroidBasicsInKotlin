d2bb0a1419e6541935ea192ceb2ef9181ff9a3f1
imsyf <alghozalyi@gmail.com>
2022-07-10 20:04:47 +0700

    Calculate tip amount

--- TipTime/src/main/java/im/syf/tiptime/MainActivity.kt
+++ TipTime/src/main/java/im/syf/tiptime/MainActivity.kt
@@ -2,10 +2,38 @@ package im.syf.tiptime
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import im.syf.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import java.util.Locale
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val input = binding.costOfService.text.toString()
+        val cost = input.toDouble()
+        val percentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty -> 0.2
+            R.id.option_eighteen -> 0.18
+            else -> 0.15
+        }
+
+        var tip = cost * percentage
+        if (binding.roundUpSwitch.isChecked) {
+            tip = ceil(tip)
+        }
+
+        val formatted = NumberFormat.getCurrencyInstance(Locale.US).format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formatted)
     }
 }
