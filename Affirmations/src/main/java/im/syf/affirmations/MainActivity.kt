251a7a8e16bb1b20c1d4af403de874018d8ab25d
imsyf <alghozalyi@gmail.com>
2022-07-12 15:11:26 +0700

    Display list of data to the screen

--- Affirmations/src/main/java/im/syf/affirmations/MainActivity.kt
+++ Affirmations/src/main/java/im/syf/affirmations/MainActivity.kt
@@ -2,10 +2,18 @@ package im.syf.affirmations
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import im.syf.affirmations.adapter.ItemAdapter
+import im.syf.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+
+        recyclerView.adapter = ItemAdapter(Datasource.loadAffirmations())
+        recyclerView.setHasFixedSize(true)
     }
 }
