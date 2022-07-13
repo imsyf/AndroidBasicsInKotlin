74430e22befe4b9d351048fa7b0e03a85c57e699
imsyf <alghozalyi@gmail.com>
2022-07-13 20:24:43 +0700

    Add Dogglers app module

---
+++ Dogglers/src/main/java/im/syf/dogglers/VerticalListActivity.kt
@@ -0,0 +1,29 @@
+package im.syf.dogglers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import im.syf.dogglers.adapter.DogCardAdapter
+import im.syf.dogglers.const.Layout
+import im.syf.dogglers.databinding.ActivityVerticalListBinding
+
+class VerticalListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVerticalListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVerticalListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.verticalRecyclerView.adapter = DogCardAdapter(
+            applicationContext,
+            Layout.VERTICAL
+        )
+
+        // Specify fixed size to improve performance
+        binding.verticalRecyclerView.setHasFixedSize(true)
+
+        // Enable up button for backward navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
