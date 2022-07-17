8dbf1ab5bbeb5de7b1ca0447f601ad0991bb28c1
imsyf <alghozalyi@gmail.com>
2022-07-17 14:34:49 +0700

    Add Cupcake app module

---
+++ Cupcake/src/main/java/im/syf/cupcake/MainActivity.kt
@@ -0,0 +1,8 @@
+package im.syf.cupcake
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Activity for cupcake order flow.
+ */
+class MainActivity : AppCompatActivity(R.layout.activity_main)
