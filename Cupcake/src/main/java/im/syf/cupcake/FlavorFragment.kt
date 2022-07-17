a4349554d15d0c0cc19d6c588ceb72755d1bdea7
imsyf <alghozalyi@gmail.com>
2022-07-17 17:16:37 +0700

    Navigate between the screens

--- Cupcake/src/main/java/im/syf/cupcake/FlavorFragment.kt
+++ Cupcake/src/main/java/im/syf/cupcake/FlavorFragment.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import im.syf.cupcake.databinding.FragmentFlavorBinding
 
 /**
@@ -39,7 +39,8 @@ class FlavorFragment : Fragment() {
      * Navigate to the next screen to choose pickup date.
      */
     fun goToNextScreen() {
-        Toast.makeText(activity, "Next", Toast.LENGTH_SHORT).show()
+        val action = FlavorFragmentDirections.toPickupFragment()
+        findNavController().navigate(action)
     }
 
     /**
