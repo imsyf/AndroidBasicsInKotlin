97f5794b46a862aab1fcd372232a01bfb67f5522
imsyf <alghozalyi@gmail.com>
2022-07-19 05:37:40 +0700

    Disable swipe gesture

--- Sports/src/main/java/im/syf/sports/SportsListFragment.kt
+++ Sports/src/main/java/im/syf/sports/SportsListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.slidingpanelayout.widget.SlidingPaneLayout
 import im.syf.sports.databinding.FragmentSportsListBinding
 
 class SportsListFragment : Fragment() {
@@ -23,6 +24,8 @@ class SportsListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentSportsListBinding.bind(view)
+
+        binding.slidingPaneLayout.lockMode = SlidingPaneLayout.LOCK_MODE_LOCKED
 
         // Connect the SlidingPaneLayout to the system back button.
         requireActivity().onBackPressedDispatcher.addCallback(
