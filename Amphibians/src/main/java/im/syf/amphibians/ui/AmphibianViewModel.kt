591bc944722c947bd173e2cd5991d2c36829dee2
imsyf <alghozalyi@gmail.com>
2022-07-21 20:26:32 +0700

    Fetch and display list of amphibians

--- Amphibians/src/main/java/im/syf/amphibians/ui/AmphibianViewModel.kt
+++ Amphibians/src/main/java/im/syf/amphibians/ui/AmphibianViewModel.kt
@@ -1,7 +1,12 @@
 package im.syf.amphibians.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import im.syf.amphibians.network.Amphibian
+import im.syf.amphibians.network.AmphibianApi
+import kotlinx.coroutines.launch
 
 enum class AmphibianApiStatus { LOADING, ERROR, DONE }
 
@@ -10,12 +15,28 @@ class AmphibianViewModel : ViewModel() {
     // TODO: Create properties to represent MutableLiveData and LiveData for the API status
 
     // TODO: Create properties to represent MutableLiveData and LiveData for a list of amphibian objects
+    private val _amphibians = MutableLiveData<List<Amphibian>>()
+    val amphibians: LiveData<List<Amphibian>> = _amphibians
 
     // TODO: Create properties to represent MutableLiveData and LiveData for a single amphibian object.
     //  This will be used to display the details of an amphibian when a list item is clicked
 
+    init {
+        getAmphibianList()
+    }
+
     // TODO: Create a function that gets a list of amphibians from the api service and sets the
     //  status via a Coroutine
+    private fun getAmphibianList() {
+        viewModelScope.launch {
+            try {
+                _amphibians.value = AmphibianApi.retrofitService.getAmphibians()
+            }
+            catch (e: Exception) {
+                _amphibians.value = emptyList()
+            }
+        }
+    }
 
     fun onAmphibianClicked(amphibian: Amphibian) {
         // TODO: Set the amphibian object
