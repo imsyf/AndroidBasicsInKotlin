81e171688ad9cc8aa13d6370c391255775c8a971
imsyf <alghozalyi@gmail.com>
2022-07-21 19:03:32 +0700

    Annotate data class to generate Moshi adapter

--- Amphibians/src/main/java/im/syf/amphibians/network/Amphibian.kt
+++ Amphibians/src/main/java/im/syf/amphibians/network/Amphibian.kt
@@ -1,10 +1,13 @@
 package im.syf.amphibians.network
 
+import com.squareup.moshi.JsonClass
+
 /**
  * This data class defines an Amphibian which includes the amphibian's name, the type of
  * amphibian, and a brief description of the amphibian.
  * The property names of this data class are used by Moshi to match the names of values in JSON.
  */
+@JsonClass(generateAdapter = true)
 data class Amphibian(
     val name: String,
     val type: String,
