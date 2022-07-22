21f4992de3ea8f4c57d6d0c3475bd0c32eaa3bb9
imsyf <alghozalyi@gmail.com>
2022-07-22 20:14:24 +0700

    Replace `GlobalScope` with smaller scope tied to Fragment lifecycle
    While it fixed earlier crash, exposing suspend functions to the UI is still not ideal.

--- BusSchedule/README.md
+++ BusSchedule/README.md
@@ -6,9 +6,17 @@ first screen will display a list of all arrival times for that particular stop.
 ## Topics covered in this module
 
 - Room
-- Kotlin Flow
+- Kotlin Coroutine: scope, context, return value
 - RecyclerView: ListAdapter with item diffing
 - Navigation: with safe args
 - ViewModel
 
 ## Result
+
+### Crash caused by `GlobalScope`
+
+[Before.webm](https://user-images.githubusercontent.com/29587914/180446185-af6e74c8-31b5-4c6e-a085-b3427355a782.webm)
+
+### The fix
+
+[After.webm](https://user-images.githubusercontent.com/29587914/180446008-5ae6efa3-b34a-40ea-9ad3-55bf6bdbc534.webm)
--- BusSchedule/src/main/java/im/syf/busschedule/FullScheduleFragment.kt
+++ BusSchedule/src/main/java/im/syf/busschedule/FullScheduleFragment.kt
@@ -6,13 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import im.syf.busschedule.databinding.FullScheduleFragmentBinding
 import im.syf.busschedule.viewmodels.BusScheduleViewModel
 import im.syf.busschedule.viewmodels.BusScheduleViewModelFactory
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class FullScheduleFragment : Fragment() {
 
@@ -43,12 +41,9 @@ class FullScheduleFragment : Fragment() {
 
         binding.recyclerView.adapter = adapter
 
-        // submitList() is a call that accesses the database. To prevent the
-        // call from potentially locking the UI, you should use a
-        // coroutine scope to launch the function. Using GlobalScope is not
-        // best practice, and in the next step we'll see how to improve this.
-        GlobalScope.launch(Dispatchers.IO) {
-            adapter.submitList(busScheduleViewModel.fullSchedule())
+        viewLifecycleOwner.lifecycleScope.launchWhenResumed {
+            val list = busScheduleViewModel.fullSchedule()  // main thread safe
+            adapter.submitList(list)
         }
     }
 
