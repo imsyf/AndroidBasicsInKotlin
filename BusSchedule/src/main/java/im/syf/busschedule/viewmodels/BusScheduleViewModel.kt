3e2cd393ea190011b24ab89c0dc6aa566ad8de79
imsyf <alghozalyi@gmail.com>
2022-07-22 20:00:58 +0700

    Make DAO suspendable to make 'em main thread safe

--- BusSchedule/src/main/java/im/syf/busschedule/viewmodels/BusScheduleViewModel.kt
+++ BusSchedule/src/main/java/im/syf/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -1,14 +1,22 @@
 package im.syf.busschedule.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import im.syf.busschedule.database.schedule.Schedule
 import im.syf.busschedule.database.schedule.ScheduleDao
+import kotlinx.coroutines.withContext
 
 class BusScheduleViewModel(
     private val scheduleDao: ScheduleDao,
 ) : ViewModel() {
 
-    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
+    suspend fun fullSchedule(): List<Schedule> =
+        withContext(viewModelScope.coroutineContext) {
+            scheduleDao.getAll()
+        }
 
-    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+    suspend fun scheduleForStopName(name: String): List<Schedule> =
+        withContext(viewModelScope.coroutineContext) {
+            scheduleDao.getByStopName(name)
+        }
 }
