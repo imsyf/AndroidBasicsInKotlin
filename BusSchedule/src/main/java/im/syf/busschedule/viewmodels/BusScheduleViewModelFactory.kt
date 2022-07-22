d9ab832d4dc8ca97d69ed18a0f121c218bbb1371
imsyf <alghozalyi@gmail.com>
2022-07-22 19:07:39 +0700

    Define view model as an intermediary between data and UI

---
+++ BusSchedule/src/main/java/im/syf/busschedule/viewmodels/BusScheduleViewModelFactory.kt
@@ -0,0 +1,18 @@
+package im.syf.busschedule.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import im.syf.busschedule.database.schedule.ScheduleDao
+
+class BusScheduleViewModelFactory(
+    private val scheduleDao: ScheduleDao,
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return BusScheduleViewModel(scheduleDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
