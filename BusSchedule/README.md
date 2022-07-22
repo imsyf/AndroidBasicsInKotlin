38c1cce5fbbdcd5ae9a04c9ae16cf30f52f0d8a5
imsyf <alghozalyi@gmail.com>
2022-07-22 10:17:07 +0700

    Add Bus Schedule app module

---
+++ BusSchedule/README.md
@@ -0,0 +1,14 @@
+# Bus Scheduler
+
+The Bus Scheduler app displays a list of bus stops and arrival times. Tapping a bus stop on the
+first screen will display a list of all arrival times for that particular stop.
+
+## Topics covered in this module
+
+- Room
+- Kotlin Flow
+- RecyclerView: ListAdapter with item diffing
+- Navigation: with safe args
+- ViewModel
+
+## Result
---
+++ BusSchedule/src/main/java/im/syf/busschedule/FullScheduleFragment.kt
@@ -0,0 +1,28 @@
+package im.syf.busschedule
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import im.syf.busschedule.databinding.FullScheduleFragmentBinding
+
+class FullScheduleFragment : Fragment() {
+
+    private var _binding: FullScheduleFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FullScheduleFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
