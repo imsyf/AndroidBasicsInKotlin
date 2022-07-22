8468c67071887f5c3dc3bfb0e7cf580b71e65abf
imsyf <alghozalyi@gmail.com>
2022-07-22 19:09:50 +0700

    Display list of data with RecyclerView

--- BusSchedule/src/main/java/im/syf/busschedule/FullScheduleFragment.kt
+++ BusSchedule/src/main/java/im/syf/busschedule/FullScheduleFragment.kt
@@ -5,12 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import im.syf.busschedule.databinding.FullScheduleFragmentBinding
+import im.syf.busschedule.viewmodels.BusScheduleViewModel
+import im.syf.busschedule.viewmodels.BusScheduleViewModelFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class FullScheduleFragment : Fragment() {
 
     private var _binding: FullScheduleFragmentBinding? = null
     private val binding get() = _binding!!
+
+    private val busScheduleViewModel: BusScheduleViewModel by activityViewModels {
+        val busScheduleApplication = activity?.application as BusScheduleApplication
+        BusScheduleViewModelFactory(busScheduleApplication.database.scheduleDao())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +31,25 @@ class FullScheduleFragment : Fragment() {
     ): View {
         _binding = FullScheduleFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = BusStopAdapter {
+            val action = FullScheduleFragmentDirections.toStopScheduleFragment(it.stopName)
+            findNavController().navigate(action)
+        }
+
+        binding.recyclerView.adapter = adapter
+
+        // submitList() is a call that accesses the database. To prevent the
+        // call from potentially locking the UI, you should use a
+        // coroutine scope to launch the function. Using GlobalScope is not
+        // best practice, and in the next step we'll see how to improve this.
+        GlobalScope.launch(Dispatchers.IO) {
+            adapter.submitList(busScheduleViewModel.fullSchedule())
+        }
     }
 
     override fun onDestroyView() {
--- BusSchedule/src/main/java/im/syf/busschedule/StopScheduleFragment.kt
+++ BusSchedule/src/main/java/im/syf/busschedule/StopScheduleFragment.kt
@@ -5,7 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import im.syf.busschedule.databinding.StopScheduleFragmentBinding
+import im.syf.busschedule.viewmodels.BusScheduleViewModel
+import im.syf.busschedule.viewmodels.BusScheduleViewModelFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class StopScheduleFragment : Fragment() {
 
@@ -15,6 +21,11 @@ class StopScheduleFragment : Fragment() {
 
     private var _binding: StopScheduleFragmentBinding? = null
     private val binding get() = _binding!!
+
+    private val busScheduleViewModel: BusScheduleViewModel by activityViewModels {
+        val busScheduleApplication = activity?.application as BusScheduleApplication
+        BusScheduleViewModelFactory(busScheduleApplication.database.scheduleDao())
+    }
 
     private lateinit var stopName: String
 
@@ -33,6 +44,21 @@ class StopScheduleFragment : Fragment() {
     ): View {
         _binding = StopScheduleFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = BusStopAdapter()
+        binding.recyclerView.adapter = adapter
+
+        // submitList() is a call that accesses the database. To prevent the
+        // call from potentially locking the UI, you should use a
+        // coroutine scope to launch the function. Using GlobalScope is not
+        // best practice, and in the next step we'll see how to improve this.
+        GlobalScope.launch(Dispatchers.IO) {
+            adapter.submitList(busScheduleViewModel.scheduleForStopName(stopName))
+        }
     }
 
     override fun onDestroyView() {
