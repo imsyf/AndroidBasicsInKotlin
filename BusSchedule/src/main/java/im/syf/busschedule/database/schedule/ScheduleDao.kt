b3a1fe2dff1542f9c37349d937ecf75d70f596a9
imsyf <alghozalyi@gmail.com>
2022-07-22 20:00:58 +0700

    Make DAO suspendable to make 'em main thread safe

--- BusSchedule/src/main/java/im/syf/busschedule/database/schedule/ScheduleDao.kt
+++ BusSchedule/src/main/java/im/syf/busschedule/database/schedule/ScheduleDao.kt
@@ -7,8 +7,8 @@ import androidx.room.Query
 interface ScheduleDao {
 
     @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
-    fun getAll(): List<Schedule>
+    suspend fun getAll(): List<Schedule>
 
     @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
-    fun getByStopName(stopName: String): List<Schedule>
+    suspend fun getByStopName(stopName: String): List<Schedule>
 }
