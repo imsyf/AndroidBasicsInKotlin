3c1144a731b2598d5604e46b9cf26154391f8093
imsyf <alghozalyi@gmail.com>
2022-07-23 13:10:57 +0700

    Pivot from Database Inspector to Android Debug Database

--- SqlBasics/src/main/java/im/syf/sqlbasics/MainActivity.kt
+++ SqlBasics/src/main/java/im/syf/sqlbasics/MainActivity.kt
@@ -1,6 +1,7 @@
 package im.syf.sqlbasics
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
@@ -13,6 +14,20 @@ class MainActivity : AppCompatActivity() {
 
         lifecycleScope.launch {
             AppDatabase.getDatabase(applicationContext).californiaParkDao().getAll()
+        }
+
+        showDebugDBAddressLogToast()
+    }
+
+    private fun showDebugDBAddressLogToast() {
+        if (BuildConfig.DEBUG) {
+            try {
+                val debugDB = Class.forName("com.amitshekhar.DebugDB")
+                val getAddressLog = debugDB.getMethod("getAddressLog")
+                val value = getAddressLog.invoke(null)
+                Toast.makeText(this, "$value", Toast.LENGTH_LONG).show()
+            } catch (e: Exception) {
+            }
         }
     }
 }
