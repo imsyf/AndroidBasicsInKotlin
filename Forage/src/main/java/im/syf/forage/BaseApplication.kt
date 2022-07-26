1e72b26da2e77c251d217664e8156cc10c247750
imsyf <alghozalyi@gmail.com>
2022-07-26 13:22:55 +0700

    Create forageable database

--- Forage/src/main/java/im/syf/forage/BaseApplication.kt
+++ Forage/src/main/java/im/syf/forage/BaseApplication.kt
@@ -1,6 +1,7 @@
 package im.syf.forage
 
 import android.app.Application
+import im.syf.forage.data.ForageDatabase
 
 /**
  * An application class that inherits from [Application], allows for the creation of a singleton
@@ -8,6 +9,5 @@ import android.app.Application
  */
 class BaseApplication : Application() {
 
-    // TODO: provide a ForageDatabase value by lazy here
-
+    val database: ForageDatabase by lazy { ForageDatabase.getDatabase(this) }
 }
