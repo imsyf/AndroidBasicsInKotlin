e1a2b52362191874993c3d839c1dcc27ccbe5a59
imsyf <alghozalyi@gmail.com>
2022-07-26 13:22:55 +0700

    Create forageable database

--- Forage/src/main/java/im/syf/forage/data/ForageDatabase.kt
+++ Forage/src/main/java/im/syf/forage/data/ForageDatabase.kt
@@ -1,5 +1,8 @@
 package im.syf.forage.data
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import im.syf.forage.model.Forageable
 
@@ -7,7 +10,36 @@ import im.syf.forage.model.Forageable
  * Room database to persist data for the Forage app.
  * This database stores a [Forageable] entity
  */
-// TODO: create the database with all necessary annotations, methods, variables, etc.
+@Database(
+    entities = [
+        Forageable::class,
+    ],
+    version = 1,
+    exportSchema = false,
+)
 abstract class ForageDatabase : RoomDatabase() {
 
+    abstract fun forageableDao(): ForageableDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: ForageDatabase? = null
+
+        fun getDatabase(context: Context): ForageDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ForageDatabase::class.java,
+                    "forageable_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                INSTANCE = instance
+
+                instance
+            }
+        }
+    }
 }
