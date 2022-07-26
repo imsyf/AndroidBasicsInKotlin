08a43a20d8d864e0efeefbf8b86e92a52b79e339
imsyf <alghozalyi@gmail.com>
2022-07-26 17:34:15 +0700

    Add Blur-O-Matic app module

---
+++ BlurOMatic/README.md
@@ -0,0 +1,11 @@
+![ic_launcher_bluromatic](src/main/res/mipmap-xhdpi/ic_launcher_bluromatic_round.png?raw=true) ![ic_launcher_bluromatic](images/Screenshot_20220726_175446.png?raw=true)
+
+# Blur-O-Matic
+
+An app that blurs photos and saves the result to a file.
+
+## Topics covered in this module
+
+- WorkManager
+
+## Result
---
+++ BlurOMatic/src/main/java/im/syf/bluromatic/BlurViewModel.kt
@@ -0,0 +1,60 @@
+package im.syf.bluromatic
+
+import android.app.Application
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class BlurViewModel(application: Application) : ViewModel() {
+
+    internal var imageUri: Uri? = null
+    internal var outputUri: Uri? = null
+
+    init {
+        imageUri = getImageUri(application.applicationContext)
+    }
+
+    /**
+     * Create the WorkRequest to apply the blur and save the resulting image
+     * @param blurLevel The amount to blur the image
+     */
+    internal fun applyBlur(blurLevel: Int) {
+    }
+
+    internal fun setOutputUri(outputImageUri: String?) {
+        outputUri = uriOrNull(outputImageUri)
+    }
+
+    private fun getImageUri(context: Context): Uri {
+        val resources = context.resources
+
+        val imageUri = Uri.Builder()
+            .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+            .authority(resources.getResourcePackageName(R.drawable.android_cupcake))
+            .appendPath(resources.getResourceTypeName(R.drawable.android_cupcake))
+            .appendPath(resources.getResourceEntryName(R.drawable.android_cupcake))
+            .build()
+
+        return imageUri
+    }
+
+    private fun uriOrNull(uriString: String?): Uri? {
+        return if (!uriString.isNullOrEmpty()) {
+            Uri.parse(uriString)
+        } else {
+            null
+        }
+    }
+
+    class Factory(private val application: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(BlurViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return BlurViewModel(application) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+}
